3a0b3bf9851b3fd88c4cc0b93e5b2da9b9eb279c
Craig Condit <ccondit@randomcoder.org>
2015-02-02 22:14:33 -0600

    Initial commit

---
+++ ymz_synth.cpp
@@ -0,0 +1,25 @@
+// Do not remove the include below
+#include "ymz_synth.h"
+
+/*
+  YMZ284 Midi
+
+  Turns a pair of Yamaha YMZ284 sound chips into a MIDI-controlled
+  instrument (or at least it will).
+
+  Created 31 Jan 2015
+  by Craig Condit
+ */
+
+#define LED 13
+
+void setup() {
+	pinMode(13, OUTPUT);
+}
+
+void loop() {
+	digitalWrite(13, HIGH);
+	delay(1000);
+	digitalWrite(13, LOW);
+	delay(1000);
+}
